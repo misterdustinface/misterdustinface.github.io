529cc7f5b712407e17341848bba7b87b9678d032
Dustin <Dustin.N.Shaffer@gmail.com>
2015-09-07 14:09:25 -0700

    Update graphics.js

--- javascripts/graphics.js
+++ javascripts/graphics.js
@@ -1,39 +1,47 @@
-var canvas = document.getElementById("gamecanvas");
-var ctx = canvas.getContext("2d");
-ctx.font = "bold 16px Monospace";
-
-function getWidth() {
-    return canvas.width;
-}
-function getHeight() {
-    return canvas.height;
+function Graphics() {
+    this.canvas = document.getElementById("gamecanvas");
+    this.ctx = canvas.getContext("2d");
+    this.ctx.font = "bold 16px Monospace";
 }
 
-function clearCanvas() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+Graphics.prototype.update = function() {
+    this.ctx = canvas.getContext("2d");
+    this.ctx.font = "bold 16px Monospace";
 }
 
-function setColor(c) {
-    ctx.fillStyle = c;
+Graphics.prototype.getWidth = function() {
+    return this.canvas.width;
 }
 
-function drawCircle(x, y, r) {
-    ctx.beginPath();
-    ctx.arc(x, y, r, 0, 2*Math.PI, false);
-    ctx.fill();
+Graphics.prototype.getHeight = function() {
+    return this.canvas.height;
 }
 
-function drawRect(x, y, w, h) {
-    ctx.beginPath();
-    ctx.rect(x, y, w, h);
-    ctx.fill();
+Graphics.prototype.clearCanvas = function() {
+    this.ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-function drawText(text, xPos, yPos) {
-    ctx.fillText(xText, xPos, yPos);
+Graphics.prototype.setColor = function(c) {
+    this.ctx.fillStyle = c;
 }
 
-function drawTextCentered(text, xPos, yPos) {
-    var length = ctx.measureText(text).width;
-    drawText(text, xPos - (length/2), yPos);
+Graphics.prototype.drawCircle = function(x, y, r) {
+    this.ctx.beginPath();
+    this.ctx.arc(x, y, r, 0, 2*Math.PI, false);
+    this.ctx.fill();
+}
+
+Graphics.prototype.drawRect = function(x, y, w, h) {
+    this.ctx.beginPath();
+    this.ctx.rect(x, y, w, h);
+    this.ctx.fill();
+}
+
+Graphics.prototype.drawText = function(text, xPos, yPos) {
+    this.ctx.fillText(xText, xPos, yPos);
+}
+
+Graphics.prototype.drawTextCentered = function(text, xPos, yPos) {
+    var length = this.ctx.measureText(text).width;
+    this.drawText(text, xPos - (length/2), yPos);
 }
