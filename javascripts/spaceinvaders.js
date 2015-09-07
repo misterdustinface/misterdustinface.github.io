9a53e2b9f5955707ea91dae2d2d0abd967a5f2d5
Dustin <Dustin.N.Shaffer@gmail.com>
2015-09-07 13:08:17 -0700

    Update spaceinvaders.js

--- javascripts/spaceinvaders.js
+++ javascripts/spaceinvaders.js
@@ -1,16 +1,15 @@
 window.onload = function() {
     window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.msRequestAnimationFrame;
     window.focus();
-    runTests();
+    $.getScript("/javscripts/TDD.js", function() {
+       window.alert("TDD library loaded");
+       runTests();
+    });
     $.getScript("/javascripts/graphics.js", function() {
         window.alert("graphics library loaded");
+        loadGame();
     });
-    loadGame();
 };
-
-var canvas = document.getElementById("gamecanvas");
-var ctx = canvas.getContext("2d");
-ctx.font = "bold 16px Monospace";
 
 var KEYS = {
     W: 87, A: 65, S: 83, D: 68,
@@ -63,11 +62,6 @@ function keyUpEventHandler(e) {
 }
 
 <!-- TESTS -->
-var TESTS = [
-    test_keyDownEventHandler_expectKeysManipulatePlayerShipUserIntData,
-    test_keyUpEventHandler_expectKeysManipulatePlayerShipUserIntData,
-];
-
 function test_keyDownEventHandler_expectKeysManipulatePlayerShipUserIntData() {
     playerShipUserIntData.shoot = false;
     playerShipUserIntData.moveLeft = false;
@@ -98,100 +92,5 @@ function test_keyUpEventHandler_expectKeysManipulatePlayerShipUserIntData() {
     expectEQ(false, playerShipUserIntData.moveRight, "moveRight should be false");
 }
 
-<!-- TEST LIBRARY -->
-function runTests() {
-    var testsysdata = { failures:[], passes:0, numTests:0 };
-    shuffleTests();
-    for (var i = 0; i < TESTS.length; i++) {
-        runTest(TESTS[i], testsysdata);
-    }
-    displayTestResults(testsysdata);
-}
-
-function shuffleTests() {
-    shuffle(TESTS);
-}
-
-function displayTestResults(testsysdata) {
-    var results = "Passed:  " + testsysdata.passes + "/" + testsysdata.numTests + "\n";
-    if (testsysdata.passes !== testsysdata.numTests) {
-        var results = results + "Failed:  \n";
-        for (var i = 0; i < testsysdata.failures.length; i++) {
-            results = results + testsysdata.failures[i] + "\n" ;
-        }
-    }
-    window.alert(results);
-}
-
-var testresult = { passed:0, failures:[] };
-function initRunTest() {
-    testresult.passed = 0;
-    testresult.failures = [];
-}
-
-function recordTestResult(testsysdata) {
-    testsysdata.numTests = testsysdata.numTests + 1;
-    if (testresult.failures.length > 0) {
-        for (var i = 0; i < testresult.failures.length; i++) {
-            testsysdata.failures.push(testresult.failures[i]);   
-        }
-    } else {
-        testsysdata.passes = testsysdata.passes + 1;
-    }
-}
-
-function runTest(xTest, testsysdata) {
-    initRunTest();
-    xTest();
-    recordTestResult(testsysdata);
-}
-
-function expectEQ(xExpected, xReceived, xLabel) {
-    var result = (xExpected === xReceived);
-    if (result) {
-        testresult.passed = testresult.passed + 1;
-    } else {
-        testresult.failures.push(xLabel);
-    }
-}
-
-<!-- GRAPHICS LIBRARY -->
-function clearCanvas() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-}
-
-function setColor(c) {
-    ctx.fillStyle = c;
-}
-
-function drawCircle(x, y, r) {
-    ctx.beginPath();
-    ctx.arc(x, y, r, 0, 2*Math.PI, false);
-    ctx.fill();
-}
-
-function drawRect(x, y, w, h) {
-    ctx.beginPath();
-    ctx.rect(x, y, w, h);
-    ctx.fill();
-}
-
-function drawText(text, xPos, yPos) {
-    ctx.fillText(xText, xPos, yPos);
-}
-
-function drawTextCentered(text, xPos, yPos) {
-    var length = ctx.measureText(text).width;
-    drawText(text, xPos - (length/2), yPos);
-}
-
-<!-- MISC -->
-function shuffle(xArray) {
-    for (var i = (xArray.length - 1); i > 0; i--) {
-        var X = Math.floor(Math.random() * i);
-        var temp  = xArray[i];
-        xArray[i] = xArray[X];
-        xArray[X] = temp;
-    }
-    return xArray;
-}
+addTest(test_keyDownEventHandler_expectKeysManipulatePlayerShipUserIntData);
+addTest(test_keyUpEventHandler_expectKeysManipulatePlayerShipUserIntData);
