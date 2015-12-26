e2b104a8ba06ae9df1e592c04877f46f26adc2cb
Dustin <Dustin.N.Shaffer@gmail.com>
2015-12-26 03:38:49 -0800

    Update menu.js

--- javascripts/menu.js
+++ javascripts/menu.js
@@ -26,7 +26,7 @@ function closeMenu() {
 
 var isMenuOpen;
 function clickMenuButton() {
-  isMenuOpen = !isMenuOpen;
+  //isMenuOpen = !isMenuOpen;
   if (isMenuOpen) {
     openMenu();
   } else {
