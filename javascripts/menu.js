9ce817211e01e24557b34cb40dbda08dc34c394b
Dustin <Dustin.N.Shaffer@gmail.com>
2015-12-26 02:35:00 -0800

    Update menu.js

--- javascripts/menu.js
+++ javascripts/menu.js
@@ -1,7 +1,11 @@
 var openMenu = function() {
-  $('.homebar').animate({
-    top: "0px";
+  $('.menubutton').animate({
+    left: "0px";
   }, 200);
+  
+  // $('.homebar').animate({
+  //   top: "0px";
+  // }, 200);
 
   //$('body').animate({
   //  top: "285px";
@@ -9,9 +13,13 @@ var openMenu = function() {
 };
 
 var closeMenu = function() {
-  $('.homebar').animate({
-    top: "-285px";
+  $('.menubutton').animate({
+    left: "200px";
   }, 200);
+  
+  // $('.homebar').animate({
+  //   top: "-285px";
+  // }, 200);
 
   //$('body').animate({
   //  top: "0px";
