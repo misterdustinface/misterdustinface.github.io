5fd5937dde3c9614e820b818434beff7ba1a9dbe
Dustin <Dustin.N.Shaffer@gmail.com>
2015-12-31 23:13:26 -0800

    Update Game.js

--- javascripts/Game.js
+++ javascripts/Game.js
@@ -13,32 +13,32 @@ function Game() {
   };
 }
 
-Graphics.prototype.load = function() {
+Game.prototype.load = function() {
   window.addEventListener("keydown", this.KEY_DOWN_FUNC);
   window.addEventListener("keyup", this.KEY_UP_FUNC);
   window.setInterval(this.UPDATE, 1000/60);
   window.setInterval(this.DRAW, 1000/60);
 }
 
-Graphics.prototype.unload = function() {
+Game.prototype.unload = function() {
   window.removeEventListener("keydown", this.KEY_DOWN_FUNC);
   window.removeEventListener("keyup", this.KEY_UP_FUNC);
   window.clearInterval(this.UPDATE);
   window.clearInterval(this.DRAW);
 }
 
-Graphics.prototype.update = function() {
+Game.prototype.update = function() {
   
 }
 
-Graphics.prototype.draw = function() {
+Game.prototype.draw = function() {
   
 }
 
-Graphics.prototype.keyDownEventHandler = function() {
+Game.prototype.keyDownEventHandler = function() {
   
 }
 
-Graphics.prototype.keyUpEventHandler = function() {
+Game.prototype.keyUpEventHandler = function() {
   
 }
