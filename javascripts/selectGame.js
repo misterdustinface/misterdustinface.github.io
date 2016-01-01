14558c0142c0eec9d12c1f2a218baf3f648d675f
Dustin <Dustin.N.Shaffer@gmail.com>
2015-12-31 18:34:45 -0800

    Create selectGame.js

---
+++ javascripts/selectGame.js
@@ -0,0 +1,22 @@
+function newButtonSelect(xLink) {
+  var link = xLink;
+  return function() {
+    var scriptTag = string.concat("<script type=", "\'", "text/javascript", "\'", " src=", "\'", link, "\'", ">", </script>);
+    $("#game").html(scriptTag);
+  };
+}
+
+function makeButton(xText, xLink){
+  var buttonElement = string.concat("<div id=", "\'", xText, "\'", ">", xText,"</div>");
+  $("#gameselector").append(buttonElement);
+  var elementID = string.concat("#", xText);
+  var select = newButtonSelect(xLink);
+  $(elementID).click(select);
+}
+
+function setSelectorButtons() {
+  makeButton('Pong', '/javascripts/pong.js');
+  makeButton('Space Invaders', '/javascripts/spaceinvaders.js');
+}
+
+$(document).ready(setSelectorButtons);
