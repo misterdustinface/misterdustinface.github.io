8cd23b498ffeb6644c84c508ea319ce9892e4dfd
Dustin <Dustin.N.Shaffer@gmail.com>
2015-12-31 18:56:18 -0800

    Update selectGame.js

--- javascripts/selectGame.js
+++ javascripts/selectGame.js
@@ -1,18 +1,18 @@
-function newLoadGameClosure(xLink) {
-  var link = xLink;
-  return function() {
-    var scriptTag = string.concat("<script type=", "'", "text/javascript", "'", " src=", "'", link, "'", ">", "</script>");
-    $("#game").html(scriptTag);
-  };
-}
+// function newLoadGameClosure(xLink) {
+//   var link = xLink;
+//   return function() {
+//     var scriptTag = string.concat("<script type=", "'", "text/javascript", "'", " src=", "'", link, "'", ">", "</script>");
+//     $("#game").html(scriptTag);
+//   };
+// }
 
-function makeButton(xText, xLink){
-  var buttonElement = string.concat("<div", " class=", "'", "menubutton", "'", " id=", "'", xText, "'", ">", xText, "</div>");
-  $("#gameselector").append(buttonElement);
-  var elementID = string.concat("#", xText);
-  var loadGame = newLoadGameClosure(xLink);
-  $(elementID).click(loadGame);
-}
+// function makeButton(xText, xLink) {
+//   var buttonElement = string.concat("<div", " class=", "'", "menubutton", "'", " id=", "'", xText, "'", ">", xText, "</div>");
+//   $("#gameselector").append(buttonElement);
+//   var elementID = string.concat("#", xText);
+//   var loadGame = newLoadGameClosure(xLink);
+//   $(elementID).click(loadGame);
+// }
 
 function setSelectorButtons() {
   var xText = 'Pong';
