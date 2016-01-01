f4706daa1af33c3d5cb449d3e8f4538b90b306a5
Dustin <Dustin.N.Shaffer@gmail.com>
2015-12-31 21:33:05 -0800

    Update selectGame.js

--- javascripts/selectGame.js
+++ javascripts/selectGame.js
@@ -1,29 +1,5 @@
-var lastClickedName;
-var lastClickedID;
-
-function select(xName) {
-  if (lastClickedName !== xName) {
-    var currentID = '#' + xName
-    $(currentID).html('[' + xName + ']');
-    restoreOldSelection();
-    rememberNewSelection(xName);
-  }
-}
-
-function restoreOldSelection() {
-  if (lastClickedID !== null) {
-    $(lastClickedID).html(lastClickedName);
-  }
-}
-
-function rememberNewSelection(xName) {
-  lastClickedName = xName;
-  lastClickedID = '#' + lastClickedName;
-}
-
-function newLoadGameClosure(xName, xLink) {
+function newLoadGameClosure(xLink) {
   return function() {
-    select(xName);
     var setScriptToLink = '<script type="text/javascript" src=' + '"' + xLink + '"' + '>' + '</script>';
     $("#game").html(setScriptToLink);
   };
@@ -33,7 +9,7 @@ function makeButton(xText, xLink) {
   var buttonElement = '<li id=' + '"' + xText + '"' + '>' + xText + '</li>';
   $("#selector-button-list").append(buttonElement);
   var elementID = '#' + xText;
-  var loadGame = newLoadGameClosure(xText, xLink);
+  var loadGame = newLoadGameClosure(xLink);
   $(elementID).click(loadGame);
 }
 
@@ -45,7 +21,6 @@ function setSelectorButtons() {
   init();
   makeButton('Ping', '/javascripts/pong.js');
   makeButton('Invaders', '/javascripts/spaceinvaders.js');
-  select('Ping');
 }
 
 $(document).ready(setSelectorButtons);
