b1039f1fd4d851484af88eb9aa633fe0e106728c
Dustin <Dustin.N.Shaffer@gmail.com>
2015-12-31 23:19:19 -0800

    Update gameselector.js

--- javascripts/gameselector.js
+++ javascripts/gameselector.js
@@ -29,10 +29,16 @@ function setGameSelections() {
   var hasGame = $('#game').length;
   if (hasGame) {
     init();
-    loadGame('/javascripts/pong.js');
-    //loadGame('/javascripts/spaceinvaders.js');
-    //makeButton('Ping', 'ping-game-selector');
-    //makeButton('Invaders', 'invaders-game-selector');
+    
+    var selections = 2;
+    var selection = Math.floor((Math.random() * selections) + 1);
+    if (selection === 1) {
+      loadGame('/javascripts/pong.js');
+      makeButton('Ping', 'ping-game-selector');
+    } else {
+      loadGame('/javascripts/spaceinvaders.js');
+      makeButton('Invaders', 'invaders-game-selector');
+    }
   }
 }
 
